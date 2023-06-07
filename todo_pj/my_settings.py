5fbc51abc956d0c922996c7059d7549896f9fff3
blanchedal <haprk83@gmail.com>
2023-06-07 10:31:44 +0900

    update

---
+++ todo_pj/my_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todolist',
+        'USER': 'root',
+        'PASSWORD': 'harper1736',
+        'HOST': 'localhost',
+        'PORT':'8000'
+    }
+}
+SECRET_KEY = 'django-insecure-xkc+gt=&^h^+egv9w@f6)f^!g$pjfuuf5j&&chl-pg$l3=7ir6'
--- todo_pj/settings.py
+++ todo_pj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+# import my_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,28 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-xkc+gt=&^h^+egv9w@f6)f^!g$pjfuuf5j&&chl-pg$l3=7ir6'
+# DATABASES = my_settings.DATABASES
+# SECRET_KEY = my_settings.SECRET_KEY
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todolist',
+        'USER': 'root',
+        'PASSWORD': 'harper1736',
+        'HOST': 'localhost',
+        'PORT':'8000'
+    }
+}
 SECRET_KEY = 'django-insecure-xkc+gt=&^h^+egv9w@f6)f^!g$pjfuuf5j&&chl-pg$l3=7ir6'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35.77.242.26']
 
 
 # Application definition
@@ -74,12 +91,18 @@ WSGI_APPLICATION = 'todo_pj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         # 'ENGINE': 'django.db.backends.sqlite3',
+#         # 'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'todolist',
+#         'USER': 'root',
+#         'PASSWORD': 'harper1736',
+#         'HOST': 'localhost',
+#         'PORT':'8000'
+#     }
+# }
 
 
 # Password validation
@@ -106,7 +129,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
